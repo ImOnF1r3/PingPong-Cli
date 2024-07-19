96d9775bb2c320d7fffe0af136d170e0b60af2df
Michele Ferrari <145187463+ImOnF1r3@users.noreply.github.com>
2024-07-19 20:24:21 +0200

    Update PingPongCli.c

--- PingPongCli.c
+++ PingPongCli.c
@@ -6,13 +6,13 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define INITIAL_GAMETICK 100000
-#define SCREEN_WIDTH 41
-#define SCREEN_HEIGHT (SCREEN_WIDTH / 2)
-#define PADDLE_SIZE 5
-#define INITIAL_BALL_SPEED 1
-#define MAX_BALL_SPEED 5
-#define SPEED_INCREASE_INTERVAL 15 // Increase speed every 15 seconds
+#define GAME_TICK 20000                      // Speed of game tick, set lower to gain speed
+#define SCREEN_WIDTH 41                      // Size battle field
+#define SCREEN_HEIGHT (SCREEN_WIDTH / 2)     //
+#define PADDLE_SIZE 3                        // Size of the paddle
+#define INITIAL_BALL_TICK 25                 // Start Speed
+#define MIN_BALL_TICK 1                      // Final Speed, don't set below the zero
+#define SPEED_INCREASE_INTERVAL 5            // Increase speed every 5 seconds
 
 struct termios orig_termios;
 
@@ -64,6 +64,7 @@ void clear() {
 }
 
 int main() {
+    srand(time(NULL)); // Initialize the random number generator
     struct winsize w;
     int norRow, norCol;
     int paddle_pos = SCREEN_HEIGHT / 2;
@@ -71,48 +72,60 @@ int main() {
     int ball_x = SCREEN_WIDTH / 2, ball_y = SCREEN_HEIGHT / 2;
     int ball_dir_x = -1, ball_dir_y = 1;
     int paused = 0;
-    int ball_speed = INITIAL_BALL_SPEED;
+    int ball_tick = INITIAL_BALL_TICK;
+    int ball_tick_counter = 0;
     time_t last_speed_increase_time = time(NULL);
-    int game_tick = INITIAL_GAMETICK;
 
     enableRawMode();
 
     while (1) {
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
         if (!paused) {
-            for (int step = 0; step < ball_speed; step++) {
+            if (ball_tick_counter >= ball_tick) {
+                ball_tick_counter = 0;
                 ball_x += ball_dir_x;
                 ball_y += ball_dir_y;
 
                 // Ball collision with paddles
-                if (ball_x == 1 && ball_y >= paddle_pos && ball_y < paddle_pos + PADDLE_SIZE) ball_dir_x = -ball_dir_x;
-                if (ball_x == SCREEN_WIDTH - 2 && ball_y >= opponent_pos && ball_y < opponent_pos + PADDLE_SIZE) ball_dir_x = -ball_dir_x;
+                if (ball_x == 1 && ball_y >= paddle_pos && ball_y < paddle_pos + PADDLE_SIZE) {
+                    ball_dir_x = -ball_dir_x;
+                    ball_dir_y = (rand() % 3) - 1; // -1, 0, or 1
+                }
+                if (ball_x == SCREEN_WIDTH - 2 && ball_y >= opponent_pos && ball_y < opponent_pos + PADDLE_SIZE) {
+                    ball_dir_x = -ball_dir_x;
+                    ball_dir_y = (rand() % 3) - 1; // -1, 0, or 1
+                }
+
                 // Ball collision with borders
                 if (ball_y == 0 || ball_y == SCREEN_HEIGHT - 1) ball_dir_y = -ball_dir_y;
                 if (ball_x < 0 || ball_x > SCREEN_WIDTH - 1) goto end_game; // Ball went out of bounds, end the game
+
+                // Move opponent paddle
+                if (opponent_pos + PADDLE_SIZE / 2 < ball_y) opponent_pos++;
+                else if (opponent_pos + PADDLE_SIZE / 2 > ball_y) opponent_pos--;
+                if (opponent_pos < 0) opponent_pos = 0;
+                if (opponent_pos > SCREEN_HEIGHT - PADDLE_SIZE) opponent_pos = SCREEN_HEIGHT - PADDLE_SIZE;
+            } else {
+                ball_tick_counter++;
             }
 
-            // Move opponent paddle
-            if (opponent_pos + PADDLE_SIZE / 2 < ball_y) opponent_pos++;
-            else if (opponent_pos + PADDLE_SIZE / 2 > ball_y) opponent_pos--;
-            if (opponent_pos < 0) opponent_pos = 0;
-            if (opponent_pos > SCREEN_HEIGHT - PADDLE_SIZE) opponent_pos = SCREEN_HEIGHT - PADDLE_SIZE;
-
-            // Increase ball speed at regular intervals
-            if (time(NULL) - last_speed_increase_time >= SPEED_INCREASE_INTERVAL && ball_speed < MAX_BALL_SPEED) {
-                ball_speed++;
+            // Gradually increase ball speed
+            if (time(NULL) - last_speed_increase_time >= SPEED_INCREASE_INTERVAL) {
+                if (ball_tick > MIN_BALL_TICK) {
+                    ball_tick--; // Decrease ball tick to increase speed
+                }
                 last_speed_increase_time = time(NULL);
-                // Gradually decrease the game tick as the ball speed increases
-                game_tick = INITIAL_GAMETICK / ball_speed;
             }
         }
 
         clear();
         norRow = (w.ws_row / 2) - SCREEN_HEIGHT / 2;
+        norRow = norRow - 2;
         norCol = (w.ws_col / 2) - SCREEN_WIDTH / 2;
         drawScreen(norRow, norCol, paddle_pos, opponent_pos, ball_x, ball_y);
 
-        printf("             Ball-pos: (%d, %d), Opp-pos: %d, Ball-speed: %d\n", ball_x, ball_y, opponent_pos, ball_speed);
+        printf("                    Ball-pos: (%d, %d), Opp-pos: %d, Ball-tick: %d\n", ball_x, ball_y, opponent_pos, ball_tick);
+        printf("                    Press [x] to exit, [p] to pause\n");
 
         if (kbhit()) {
             char c = getchar();
@@ -121,11 +134,11 @@ int main() {
                 char arrow_key = getchar();
                 if (arrow_key == 'A' && paddle_pos > 0) paddle_pos--;
                 else if (arrow_key == 'B' && paddle_pos < SCREEN_HEIGHT - PADDLE_SIZE) paddle_pos++;
-            } else if (c == 'x') break; // x
-            else if (c == 'p') paused = !paused; // p
+            } else if (c == 'x') break;          // x - e(X)it
+            else if (c == 'p') paused = !paused; // p - (P)ause
         }
 
-        usleep(game_tick);
+        usleep(GAME_TICK);
     }
 
 end_game:
